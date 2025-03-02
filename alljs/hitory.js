e6f9169cfff4516a80562b692a1f6982f26792be
SheikhSalman <assalmanmuhammad@gmail.com>
2025-03-02 23:52:02 +0600

    last commit

--- alljs/hitory.js
+++ alljs/hitory.js
@@ -2,11 +2,6 @@
 document.getElementById('History').addEventListener('click',function(){
     const container = document.getElementById('inbox');
     container.remove();
-    const allBtn = document.querySelectorAll(".completed-btn")
-    for(const btn of allBtn){
-            btn.disabled = false
-            btn.style.opacity = "0.9"
-        }
-        location.reload();
+        // location.reload();
     })
 
